1603398685580a8696f57ee390e2587449092362
army3145 <76448948+army3145@users.noreply.github.com>
2021-02-16 20:50:08 -0600

    Update script.js

--- script.js
+++ script.js
@@ -100,7 +100,8 @@ function buildAndShowHomeHTML (categories) {
       var request chosenCategoryShortName();
          request.onreadystatechange = 
            function() { 
-           
+             handlerResponse(chosenRandomCatergory);
+         };
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
